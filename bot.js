51f71a7b7630069137e317010f96f75d640aa431
Oleg Zlomanov <olegzlomanov@MacBook-Pro-Oleg.local>
2026-08-17 13:23:24 +0300

    Update to actual version

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
 
-// Актуальный URL вашего веб-приложения Google Apps Script (Версия 3)
-const BOT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwPUdxMacIQYJ5N-xuZ1b_uLCElzB4hVBljor6urnmTHg3Idl79Bu52e2PMMc_rTZx6bQ/exec";
+// Актуальный URL вашего веб-приложения Google Apps Script (Версия 4)
+const BOT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzWGs6aS01pteGoBSebUNBHNsKv_BL82e1QOGjYPepl2bZQov1zeR-zXasv8H4yQPo0yA/exec";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
